532d7b0e7810bddc36aefb3dd1dd8ab282060306
Craig <reeseca90@gmail.com>
2021-09-14 15:53:10 -0400

    Fully functioning version of the basic game played via console

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 let playerselection = '';
 let computerSelection = computerPlay();
 let result = '';
+let playerScore = 0;
+let computerScore = 0;
 
 // console.log(playerSelection);
 // console.log(computerSelection);
@@ -9,12 +11,22 @@ function game() {
     computerSelection = computerPlay();
     playerSelection = prompt('Enter "rock", "paper", or "scissors"', 'rock').toUpperCase();
 
-    console.log(computerSelection);
-    console.log(playerSelection);
+    // console.log(computerSelection);
+    // console.log(playerSelection);
 
     playRound(playerSelection, computerSelection);
 
     console.log(result);
+
+    if (playerScore === 5) {
+        console.log("You've won five games!");
+        resetScore();
+    }
+
+    if (computerScore === 5) {
+        console.log("You've lost five games!");
+        resetScore();
+    }
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -27,26 +39,32 @@ function playRound(playerSelection, computerSelection) {
             result = 'It\'s a tie!';
         } else if (computerSelection === 'PAPER') {
             result = 'You lose! Paper beats Rock!';
+            computerScore++;
         } else {
             result = 'You win! Rock beats Scissors!';
+            playerScore++;
         }
 
     } else if (playerSelection === 'PAPER') {
 
         if (computerSelection === 'ROCK') {
             result = 'You win! Paper beats Rock!';
+            playerScore++;
         } else if (computerSelection === 'PAPER') {
             result = 'It\'s a tie!';
         } else {
             result = 'You lose! Scissors beat Paper!';
+            computerScore++;
         }
 
     } else {
 
         if (computerSelection === 'ROCK') {
             result = 'You lose! Rock beats Scissors!';
+            computerScore++;
         } else if (computerSelection === 'PAPER') {
             result = 'You win! Scissors beat Paper!';
+            playerScore++;
         } else {
             result = 'It\'s a tie!';
         }
@@ -68,3 +86,7 @@ function computerPlay() {
     }
 }
 
+function resetScore() {
+    playerScore = 0;
+    computerScore = 0;
+}
